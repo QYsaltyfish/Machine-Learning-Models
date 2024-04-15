6fcefe590fdf888897a6b8ddb162edec0fa3fd04
QYsaltyfish <163826188+QYsaltyfish@users.noreply.github.com>
2024-04-15 15:07:54 +0800

    Add files via upload

--- models.py
+++ models.py
@@ -188,11 +188,10 @@ class ClassificationModels(Models):
         self._y = None
 
         if pre_process:
-            self._preprocess_y()
+            self._class_num = self._preprocess_y()
         else:
             self._y = self.y
-
-        self._class_num = len(np.unique(self._y))
+            self._class_num = len(np.unique(self._y))
 
     def _preprocess_y(self):
         y_dict = dict()
@@ -202,6 +201,7 @@ class ClassificationModels(Models):
                 y_dict[y[0]] = len(y_dict)
             else:
                 self._y[i][0] = y_dict[y[0]]
+        return len(y_dict)
     
     @property
     def class_num_(self):
@@ -333,3 +333,271 @@ class LogisticRegression(ClassificationModels):
     @staticmethod
     def _sigmoid(x):
         return 1 / (1 + np.e ** -x)
+
+
+class SVM(ClassificationModels):
+
+    def __init__(self, X, y, pre_process=True, margin='soft', C=1, eps=1e-3):
+        """
+        Initialize the SVM model with given parameters.
+
+        The SVM is solved by Sequential Minimal Optimization(SMO) algorithm.
+
+        Note1: Hard-margin SVM hasn't been done yet and please use margin='soft', C=np.inf.
+        If you intend to use hard-margin, please ensure that the data is perfectly linearly separable;
+        otherwise, the program may enter an infinite loop.
+
+        Note2: The heuristic function has not been actually utilized and may be employed in future version
+        to enhance efficiency.
+
+        :param X: The attribute (feature) vector
+        :param y: The label (response) vector
+        :param pre_process: Whether to preprocess vector y
+        :param margin: The type of margin of SVM model (hard or soft)
+        :param C: Coefficient for soft-margin
+        :param eps: Tolerance for convergence criteria
+        """
+        super().__init__(X, y)
+
+        if self._class_num == 1:
+            raise Exception('There is only one group of y')
+        elif self._class_num == 2:
+            self._coef = np.zeros(self._X_shape[1])
+            self._intercept = 0
+            if pre_process:
+                self._preprocess_y()
+        else:
+            raise NotImplementedError("SVM doesn't support multiple classes of y.")
+
+        if margin == 'hard':
+            self._margin = 0
+            warnings.warn("Hard-margin SVM hasn't been done yet")
+        elif margin == 'soft':
+            self._margin = 1
+        else:
+            raise Exception(f"Unexpected margin type: {margin}")
+
+        self._X_dot = np.zeros((self._X_shape[0], self._X_shape[0]))
+        self.C = C
+        self.eps = eps
+        self._b = 0
+        self._d = np.zeros(self._X_shape[0])
+        self._E = np.zeros(self._X_shape[0])
+        self._alpha = np.zeros(self._X_shape[0])
+
+    def _preprocess_y(self):
+        y_dict = dict()
+
+        if any(self._y == [0]):
+            for y in self._y:
+                if y[0] == 0:
+                    y[0] = -1
+            return
+
+        for i, y in enumerate(self._y):
+            if y[0] not in y_dict:
+                if len(y_dict) == 0:
+                    y_dict[y[0]] = -1
+                else:
+                    y_dict[y[0]] = 1
+            self._y[i][0] = y_dict[y[0]]
+
+    def fit(self):
+        for i in range(self._X_shape[0]):
+            for j in range(self._X_shape[0]):
+                self._X_dot[i][j] = np.dot(self.X[i], self.X[j])
+
+        for i in range(self._X_shape[0]):
+            self._E[i] = -self._y[i][0]
+
+        if self._margin == 0:
+            self._hard_smo_solver()
+        elif self._margin == 1:
+            self._soft_smo_solver()
+
+    def _hard_smo_solver(self):
+        # TODO
+        pass
+
+    def _soft_smo_solver(self):
+        # TODO: Apply the heuristic function
+        # Repeat the traversal pattern until no sample violates KKT conditions
+        while True:
+            # First traversal, traverse the whole training set and find the first sample violating KKT conditions.
+            find_violator = False
+            for i in range(self._X_shape[0]):
+                if self._check_epsilon_kkt_violation(i):
+                    find_violator = True
+                    l = list(range(self._X_shape[0]))
+                    j = np.random.choice(l[:i] + l[i + 1:])
+                    # j = self._find_second_alpha(i)
+                else:
+                    continue
+                new_alpha2, L, H = self._clip_alpha2(i, j, self._get_unclipped_alpha2(i, j))
+                new_alpha1 = self._alpha[i] + self._y[i][0] * self._y[j][0] * (self._alpha[j] - new_alpha2)
+                new_b = self._get_new_b(i, j, new_alpha1, new_alpha2, L, H)
+                self.update_E(i, j, new_alpha1, new_alpha2, new_b)
+                self._b = new_b
+                self._alpha[i] = new_alpha1
+                self._alpha[j] = new_alpha2
+
+            # No violators and we break
+            if not find_violator:
+                break
+            find_violator = False
+
+            # Find all the non-bound samples
+            non_bound_indexs = []
+            for i in range(self._X_shape[0]):
+                if 0 < self._alpha[i] < self.C: non_bound_indexs.append(i)
+
+            # Second traversal, traverse all the non-bound samples and optimize, until all non-bound samples
+            # satisfy KKT conditions.
+            while True:
+                find_violator_this_loop = False
+                for i in non_bound_indexs:
+                    if self._check_epsilon_kkt_violation(i):
+                        find_violator_this_loop = True
+                        find_violator = True
+                        l = list(range(self._X_shape[0]))
+                        j = np.random.choice(l[:i] + l[i + 1:])
+                        # j = self._find_second_alpha(i)
+                    else:
+                        continue
+                    new_alpha2, L, H = self._clip_alpha2(i, j, self._get_unclipped_alpha2(i, j))
+                    new_alpha1 = self._alpha[i] + self._y[i][0] * self._y[j][0] * (self._alpha[j] - new_alpha2)
+                    new_b = self._get_new_b(i, j, new_alpha1, new_alpha2, L, H)
+                    self.update_E(i, j, new_alpha1, new_alpha2, new_b)
+                    self._b = new_b
+                    self._alpha[i] = new_alpha1
+                    self._alpha[j] = new_alpha2
+                if not find_violator_this_loop:
+                    break
+
+            # No violators and we break
+            if not find_violator:
+                break
+
+        self._coef = sum((self._alpha[i] * self._y[i][0] * self.X[i] for i in range(self._X_shape[0])))
+        b_star = [self._y[j][0] -
+                  sum(self._alpha[i] * self._y[i] * self._X_dot[i][j]
+                      for i in range(self._X_shape[0]) if self._alpha[i] != 0)
+                  for j in range(self._X_shape[0]) if 0 < self._alpha[j] < self.C]
+        self._intercept = np.mean(b_star)
+
+    def _check_epsilon_kkt_violation(self, i):
+        """
+        Check the KKT conditions within precision epsilon.
+
+        If     alpha_i = 0, then 1 - eps <= y_i * d_i
+        If 0 < alpha_i < C, then 1 - eps <= y_i * d_i <= 1 + eps
+        If     alpha_i = C, then            y_i * d_i <= 1 + eps
+
+        :param i: The index of the coefficient to check KKT condition.
+        :return: Whether the coefficient **violates** epsiolon-KKT condition.
+        """
+        return (self._alpha[i] < self.C and self._y[i][0] * self._E[i] < -self.eps
+                or
+                self._alpha[i] > 0 and self._y[i][0] * self._E[i] > self.eps)
+
+    def _find_second_alpha(self, i):
+        """
+        After selecting the first variable alpha1 to optimize, choose the second variable alpha2 using heuristic
+        function.
+
+        The heuristic function is::
+
+            h(i, j) = |E_i - E_j|
+
+        :param i: The index of the first alpha
+        :return: The index of the second alpha
+        """
+        return max((j for j in range(self._X_shape[0]) if j != i), key=lambda j: abs(self._E[i] - self._E[j]))
+
+    def _get_unclipped_alpha2(self, i, j):
+        """
+        Get the unclipped value of alpha_j by optimizting both alpha_i and alpha_j in the dual problem.
+
+        The update rule is::
+
+            alpha_j (unclipped) = alpha_j (old) + y_2 * (E_i - E_j) / (k_ii + k_jj - 2k_ij)
+
+        :param i: The index of the first alpha
+        :param j: The index of the second alpha
+        :return: Unclipped alpha_j
+        """
+        eta = self._X_dot[i][i] + self._X_dot[j][j] - 2 * self._X_dot[i][j]
+        if eta > 0:
+            return self._alpha[j] + self._y[j][0] * (self._E[i] - self._E[j]) / eta
+        else:
+            return None
+
+    def _clip_alpha2(self, i, j, unclipped_alpha2):
+        """
+        Get the clipped value of alpha_j.
+
+        The upper bound of alpha_j, H, is::
+
+            H = min(C, alpha_i + alpha_j),     if y_i == y_j
+                min(C, alpha_j - alpha_i + C), if y_i != y_j
+
+        The lower bound of alpha_j, L, is::
+            L = max(0, alpha_i + alpha_j - C), if y_i == y_j
+                max(0, alpha_j - alpha_i),     if y_i != y_j
+
+        The clipping rule for alpha_j is::
+
+                                              H, if H <= alpha_j (unclipped)
+            alpha_j (new) = alpha_j (unclipped), if L <  alpha_j (unclipped) <  H
+                                              L, if      alpha_j (unclipped) <= L
+
+        :param unclipped_alpha2: The unclipped value of alpha_j
+        :return: The clipped value of alpha_j
+        """
+        if self._y[i][0] == self._y[j][0]:
+            H = min(self.C, self._alpha[i] + self._alpha[j])
+            L = max(0, self._alpha[i] + self._alpha[j] - self.C)
+        else:
+            H = min(self.C, self._alpha[j] - self._alpha[i] + self.C)
+            L = max(0, self._alpha[j] - self._alpha[i])
+
+        if unclipped_alpha2 >= H:
+            return H, L, H
+        if unclipped_alpha2 <= L:
+            return L, L, H
+        return unclipped_alpha2, L, H
+
+    def _get_new_b(self, i, j, new_alpha1, new_alpha2, L, H):
+        """
+        Get the new intercept according to new_alpha_i and new_alpha_j. The formulas are too complex and are
+        omitted here.
+        """
+        if 0 < new_alpha1 < self.C:
+            return (-self._E[i] - self._y[i][0] * self._X_dot[i][i] * (new_alpha1 - self._alpha[i]) -
+                    self._y[j][0] * self._X_dot[i][j] * (new_alpha2 - self._alpha[j]) + self._b)
+        if 0 < new_alpha2 < self.C:
+            return (-self._E[j] - self._y[i][0] * self._X_dot[i][j] * (new_alpha1 - self._alpha[i]) -
+                    self._y[j][0] * self._X_dot[j][j] * (new_alpha2 - self._alpha[j]) + self._b)
+        if L != H:
+            b1 = (-self._E[i] - self._y[i][0] * self._X_dot[i][i] * (new_alpha1 - self._alpha[i]) -
+                  self._y[j][0] * self._X_dot[i][j] * (new_alpha2 - self._alpha[j]) + self._b)
+            b2 = (-self._E[j] - self._y[i][0] * self._X_dot[i][j] * (new_alpha1 - self._alpha[i]) -
+                  self._y[j][0] * self._X_dot[j][j] * (new_alpha2 - self._alpha[j]) + self._b)
+            return 0.5 * (b1 + b2)
+        return self._b
+
+    def update_E(self, i, j, new_alpha1, new_alpha2, new_b):
+        delta_b = new_b - self._b
+        for k, old_E in enumerate(self._E):
+            self._E[k] = (old_E +
+                          self._y[i][0] * self._X_dot[i][k] * (new_alpha1 - self._alpha[i]) +
+                          self._y[j][0] * self._X_dot[j][k] * (new_alpha2 - self._alpha[j]) +
+                          delta_b)
+
+    @property
+    def coef_(self):
+        return self._coef
+
+    @property
+    def intercept_(self):
+        return self._intercept
